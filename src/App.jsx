57d730fda06701321294383206b5b8e8a31aab63
yungan9 <geyun105@naver.com>
2024-11-04 00:39:39 +0900

    Feat: Set role and content for messages

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,26 @@
+import { useState } from "react";
 import { CallGPT } from "./api/gpt";
 
 function App() {
+  const [data, setData] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+
   const handleClickAPICall = async () => {
-    await CallGPT();
+    try {
+      setIsLoading(true);
+      const message = await CallGPT();
+      setData(message);
+    } catch (error) {
+    } finally {
+      setIsLoading(false);
+    }
   };
 
   return (
     <>
       <button onClick={handleClickAPICall}>gpt api call</button>
+      <div>data:{data}</div>
+      <div>isLoading:{isLoading ? "loading..." : "fin"}</div>
     </>
   );
 }
--- src/api/gpt.js
+++ src/api/gpt.js
@@ -1,13 +1,47 @@
-export const CallGPT = async () => {
-  console.log("test");
-  // curl https://api.openai.com/v1/chat/completions \
-  // -H "Content-Type: application/json" \
-  // -H "Authorization: Bearer $OPENAI_API_KEY" \
-  // -d '{
-  //    "model": "gpt-4o-mini",
-  //    "messages": [{"role": "user", "content": "Say this is a test!"}],
-  //    "temperature": 0.7
-  //  }'
+export const CallGPT = async ({ prompt }) => {
+  const messages = [
+    {
+      role: "system",
+      content: `## INFO ##
+you can add images to the reply by URL, Write the image in JSON field 
+Use the Unsplash API (https://source.unsplash.com/1600x900/?). the query is just some tags that describes the image ## DO NOT RESPOND TO INFO BLOCK ##`,
+    },
+    {
+      role: "system",
+      content: `You are a psychological counselor who writes and analyzes emotional diaries. Proceed in the following order.`,
+    },
+    {
+      role: "user",
+      content: `1. [title] : Think of the diary title after understanding the [events] separated by """ at the bottom.
+      2. [summarize] : summarize events in order with one line sentence.
+      3. [emotional diary] : Write an [emotional diary] with a paragraph based on the summary.
+      4. [evaluates] : The written emotional [evaluates], using explore the unconscious based on the contents of the [emotional diary].
+      6. [Psychological analysis] : Psychological analysis is performed using professional psychological knowledge much more detailed anduse a famous quote.
+      7. [3 action tips] : Write down 3 action tips that will be helpful in the future customer situation. The three action tips must beconverted into JSON Array format.
+      8. [image] : Create an image by making the contents so far into one keyword.
+
+
+      Translate into Korean and Use the output in the following JSON format:
+      { 
+          title: here is [title],
+          thumbnail: here is [image],
+          summary: here is [summarize]
+          emotional_content: here is [emotional diary],
+          emotional_result: here is [evaluates],
+          analysis: here is [Psychological analysis],
+          action_list: here is [3 action tips],
+      }
+      [events]:`,
+    },
+    {
+      role: "user",
+      content: `
+      """
+      ${prompt}
+      """
+      `,
+    },
+  ];
 
   const res = await fetch("https://api.openai.com/v1/chat/completions", {
     method: "POST",
@@ -17,7 +51,7 @@ export const CallGPT = async () => {
     },
     body: JSON.stringify({
       model: "gpt-4o-mini",
-      messages: [{ role: "user", content: "Say this is a test!" }],
+      messages,
       temperature: 0.7,
       max_tokens: 1_000,
     }),
@@ -25,5 +59,7 @@ export const CallGPT = async () => {
   const resData = await res.json();
   console.log(">>resData", resData);
 
-  return resData;
+  const message = resData.choices[0].message.content;
+
+  return message;
 };
